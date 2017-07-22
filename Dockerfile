7a49994093eb66a7ff6b8a2c58a55060c2c21796
Daniel Petisme <daniel.petisme@gmail.com>
2017-07-22 17:04:05 +0200

    initial work

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+###
+# vert.x docker example using a Java verticle packaged as a fatjar
+# To build:
+#  docker build -t sample/vertx-java-fat .
+# To run:
+#   docker run -t -i -p 8080:8080 sample/vertx-java-fat
+###
+
+FROM java:openjdk-8-jdk
+
+ENV CONF_FILE vertx-starter-main/conf/default-conf.json
+ENV VERTICLE_FILE vertx-starter-main/target/vertx-starter-main-1.0.0-SNAPSHOT-fat.jar
+
+COPY . .
+RUN ./mvnw clean package
+EXPOSE 8080
+
+ENTRYPOINT ["sh", "-c"]
+CMD ["exec java -jar -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory $VERTICLE_FILE -conf $CONF_FILE"]
---
+++ vertx-starter-web/src/main/java/io/vertx/starter/web/rest/VersionResource.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2017 The original author or authors
+ * ------------------------------------------------------
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ *
+ *     The Eclipse Public License is available at
+ *     http://www.eclipse.org/legal/epl-v10.html
+ *
+ *     The Apache License v2.0 is available at
+ *     http://www.opensource.org/licenses/apache2.0.php
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ */
+package io.vertx.starter.web.rest;
+
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.web.RoutingContext;
+
+import static io.vertx.starter.web.util.RestUtil.error;
+import static io.vertx.starter.web.util.RestUtil.respondJson;
+
+public class VersionResource {
+
+    private final Logger log = LoggerFactory.getLogger(VersionResource.class);
+
+    private EventBus eventBus;
+
+    public VersionResource(EventBus eventBus) {
+        this.eventBus = eventBus;
+    }
+
+    public void findAll(RoutingContext rc) {
+        log.debug("REST request to get all Versions");
+        eventBus.send("version.query", new JsonObject(), ar -> {
+            if (ar.succeeded()) {
+                respondJson(rc, (JsonArray) ar.result().body());
+            } else {
+                error(rc, ar.cause());
+            }
+        });
+    }
+}
