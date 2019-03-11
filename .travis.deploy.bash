61045849bae44f3c56eb8ae1d66107de32bc5399
Thomas Segismont <tsegismo@redhat.com>
2019-03-11 18:03:38 +0100

    Travis: deploy script expects starter jar in /tmp/vertx directory

--- .travis.deploy.bash
+++ .travis.deploy.bash
@@ -4,7 +4,8 @@ echo "Extracting Starter version..."
 STARTER_VERSION=$(./gradlew properties -q | grep "^version:" | awk '{print $2}')
 echo "Starter version is ${STARTER_VERSION}"
 
-STARTER_JAR="./build/libs/vertx-starter-${STARTER_VERSION}-fat.jar"
+STARTER_JAR_NAME="vertx-starter-${STARTER_VERSION}-fat.jar"
+STARTER_JAR="./build/libs/${STARTER_JAR_NAME}"
 
 if [ ! -f ${STARTER_JAR} ]; then
   echo "Could not find file ${STARTER_JAR}"
@@ -17,7 +18,8 @@ REMOTE_TEMP="/tmp/vertx"
 
 echo "Sending JAR to ${REMOTE_USER}@${REMOTE_HOSTNAME}..."
 ssh "${REMOTE_USER}"@"${REMOTE_HOSTNAME}" rm -rf "${REMOTE_TEMP}"
-scp "${STARTER_JAR}" "${REMOTE_USER}"@"${REMOTE_HOSTNAME}":"${REMOTE_TEMP}"
+ssh "${REMOTE_USER}"@"${REMOTE_HOSTNAME}" mkdir -p "${REMOTE_TEMP}"
+scp "${STARTER_JAR}" "${REMOTE_USER}"@"${REMOTE_HOSTNAME}":"${REMOTE_TEMP}/${STARTER_JAR_NAME}"
 echo "JAR sent"
 
 echo "Deploying with ${REMOTE_USER}@${REMOTE_HOSTNAME}..."
