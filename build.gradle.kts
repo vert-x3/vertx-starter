94e777c876cbddd2c37a37c3da62f1f2a9015623
Daniel Petisme <daniel.petisme@gmail.com>
2019-03-13 17:42:53 +0100

    Fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.gradle.api.tasks.wrapper.Wrapper.DistributionType.ALL
 
 plugins {
@@ -26,21 +27,23 @@ repositories {
 }
 
 group = "io.vertx"
-version = "0.5.0-SNAPSHOT"
+version = "2.0.1-SNAPSHOT"
 description = "A web application to generate Vert.x projects"
 
+val vertxVersion = "3.6.3"
+
 dependencies {
-  implementation("io.vertx:vertx-web:3.6.3")
-  implementation("io.vertx:vertx-mongo-client:3.6.3")
-  implementation("io.vertx:vertx-web-client:3.6.3")
-  implementation("io.vertx:vertx-web-templ-freemarker:3.6.3")
+  implementation("io.vertx:vertx-web:${vertxVersion}")
+  implementation("io.vertx:vertx-mongo-client:${vertxVersion}")
+  implementation("io.vertx:vertx-web-client:${vertxVersion}")
+  implementation("io.vertx:vertx-web-templ-freemarker:${vertxVersion}")
 
   implementation("org.apache.commons:commons-compress:1.18")
   implementation("ch.qos.logback:logback-classic:1.2.3")
   implementation("io.github.jponge:vertx-boot:1.0.0")
 
   testImplementation("org.assertj:assertj-core:3.10.0")
-  testImplementation("io.vertx:vertx-junit5:3.6.3")
+  testImplementation("io.vertx:vertx-junit5:${vertxVersion}")
   testImplementation("com.julienviet:childprocess-vertx-ext:1.3.0")
 
   //When present gradle test work but intellij not and vice-versa
@@ -48,7 +51,7 @@ dependencies {
 }
 
 vertx {
-  vertxVersion = "3.6.3"
+  vertxVersion = vertxVersion
   mainVerticle = "io.github.jponge.vertx.boot.BootVerticle"
   jvmArgs = listOf("-Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory")
 }
@@ -58,6 +61,11 @@ tasks.withType<Test> {
   failFast = true
   jvmArgs("-Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory")
 }
+
+tasks.withType<ShadowJar> {
+  archiveFileName.set("${project.name}-${project.version}-fat.jar")
+}
+
 
 tasks.withType<Wrapper> {
   gradleVersion = "5.2.1"
