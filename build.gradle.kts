a41282af513de1e963ce700224f8b01a0155e418
Thomas Segismont <tsegismo@redhat.com>
2019-03-27 14:36:35 +0100

    Gradle build file updates after review

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,11 +14,9 @@
  * limitations under the License.
  */
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import org.gradle.api.JavaVersion.VERSION_1_8
 import org.gradle.api.tasks.wrapper.Wrapper.DistributionType.ALL
 
 plugins {
-  java
   id("io.vertx.vertx-plugin") version "0.4.0"
 }
 
@@ -31,22 +29,21 @@ group = "io.vertx"
 version = "2.0.3"
 description = "A web application to generate Vert.x projects"
 
-val vertxVersion = "3.6.3"
 val junitJupiterVersion = "5.4.1"
 val testContainersVersion = "1.11.1"
 
 dependencies {
-  implementation("io.vertx:vertx-web:${vertxVersion}")
-  implementation("io.vertx:vertx-mongo-client:${vertxVersion}")
-  implementation("io.vertx:vertx-web-client:${vertxVersion}")
-  implementation("io.vertx:vertx-web-templ-freemarker:${vertxVersion}")
+  implementation("io.vertx:vertx-web")
+  implementation("io.vertx:vertx-mongo-client")
+  implementation("io.vertx:vertx-web-client")
+  implementation("io.vertx:vertx-web-templ-freemarker")
 
   implementation("org.apache.commons:commons-compress:1.18")
   implementation("ch.qos.logback:logback-classic:1.2.3")
   implementation("io.github.jponge:vertx-boot:1.0.0")
 
   testImplementation("org.assertj:assertj-core:3.10.0")
-  testImplementation("io.vertx:vertx-junit5:${vertxVersion}")
+  testImplementation("io.vertx:vertx-junit5")
   testImplementation("org.testcontainers:testcontainers:${testContainersVersion}")
   testImplementation("com.julienviet:childprocess-vertx-ext:1.3.0")
 
@@ -55,13 +52,8 @@ dependencies {
   testImplementation("org.testcontainers:junit-jupiter:${testContainersVersion}")
 }
 
-java {
-  sourceCompatibility = VERSION_1_8
-  targetCompatibility = VERSION_1_8
-}
-
 vertx {
-  vertxVersion = vertxVersion
+  vertxVersion = "3.6.3"
   mainVerticle = "io.github.jponge.vertx.boot.BootVerticle"
   jvmArgs = listOf("-Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory")
 }
