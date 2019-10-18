bf68a0c42dfd3017388d7aa594d343cbd640be5c
Thomas Segismont <tsegismo@redhat.com>
2019-10-18 09:42:22 +0200

    Release 3.8.3
    (cherry picked from commit db330ce8b887a907733c5b05ad4a38f49fdb725b)

--- src/test/java/io/vertx/starter/service/GeneratorTest.java
+++ src/test/java/io/vertx/starter/service/GeneratorTest.java
@@ -102,7 +102,7 @@ class GeneratorTest {
       .setArtifactId("demo")
       .setLanguage(JAVA)
       .setBuildTool(MAVEN)
-      .setVertxVersion("3.8.2")
+      .setVertxVersion("3.8.3")
       .setVertxDependencies(new HashSet<>(Collections.singleton("vertx-web")))
       .setArchiveFormat(TGZ)
       .setJdkVersion(JdkVersion.JDK_1_8);
