cc51532d1591cac40a66abbfac510aa3cc0a09ee
Thomas Segismont <tsegismo@redhat.com>
2020-03-31 11:02:22 +0200

    Vert.x 3.9.0 release (#119)

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
 }
 
 vertx {
-  vertxVersion = "3.8.5"
+  vertxVersion = "3.9.0"
   mainVerticle = "io.github.jponge.vertx.boot.BootVerticle"
   jvmArgs = listOf("-Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory")
 }
