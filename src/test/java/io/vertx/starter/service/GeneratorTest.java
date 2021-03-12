59ed6d278ff54c00828791f366455e2208ea1968
Thomas Segismont <tsegismo@redhat.com>
2021-03-12 19:02:27 +0100

    4.0.3 Release

--- src/test/java/io/vertx/starter/service/GeneratorTest.java
+++ src/test/java/io/vertx/starter/service/GeneratorTest.java
@@ -112,7 +112,7 @@ class GeneratorTest {
       .setArtifactId("demo")
       .setLanguage(JAVA)
       .setBuildTool(MAVEN)
-      .setVertxVersion("4.0.2")
+      .setVertxVersion("4.0.3")
       .setArchiveFormat(TGZ)
       .setJdkVersion(JdkVersion.JDK_1_8);
   }
