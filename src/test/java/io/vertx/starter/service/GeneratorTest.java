14dbd536ab095c2f0fa7054be0e28f73a6905957
Thomas Segismont <tsegismont@gmail.com>
2021-09-17 09:59:49 +0200

    Release 4.1.4
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- src/test/java/io/vertx/starter/service/GeneratorTest.java
+++ src/test/java/io/vertx/starter/service/GeneratorTest.java
@@ -121,7 +121,7 @@ class GeneratorTest {
       .setArtifactId("demo")
       .setLanguage(JAVA)
       .setBuildTool(MAVEN)
-      .setVertxVersion("4.1.3")
+      .setVertxVersion("4.1.4")
       .setArchiveFormat(TGZ)
       .setJdkVersion(JdkVersion.JDK_1_8);
   }
