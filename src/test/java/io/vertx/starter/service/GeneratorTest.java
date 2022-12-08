fffa4d5bb73aa95ab16b26ccbdb5584618afd59e
Julien Viet <julien@julienviet.com>
2022-12-08 18:09:44 +0100

    Set version to 4.3.6

--- src/test/java/io/vertx/starter/service/GeneratorTest.java
+++ src/test/java/io/vertx/starter/service/GeneratorTest.java
@@ -121,7 +121,7 @@ class GeneratorTest {
       .setArtifactId("demo")
       .setLanguage(JAVA)
       .setBuildTool(MAVEN)
-      .setVertxVersion("4.3.5")
+      .setVertxVersion("4.3.6")
       .setArchiveFormat(TGZ)
       .setJdkVersion(JdkVersion.JDK_1_8);
   }
