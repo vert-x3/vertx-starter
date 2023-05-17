ed0b2b6667323a2fe08f9cdf7afbc1041b5e99ff
Thomas Segismont <tsegismont@gmail.com>
2023-05-17 15:42:31 +0200

    Remove normalize
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- src/main/frontend/src/main.js
+++ src/main/frontend/src/main.js
@@ -1,7 +1,6 @@
 import '@fontsource/ubuntu/500.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './assets/stylesheets/main.css'
 import './assets/main.css'
 
 import 'bootstrap'
