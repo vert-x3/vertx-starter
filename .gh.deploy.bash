4fc0eb22bf11bc22213ead05a4acd0bffdcab707
Thomas Segismont <tsegismont@gmail.com>
2023-05-30 17:13:29 +0200

    Updated deployment config
    Also, removed files that are no longer used.
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- .gh.deploy.bash
+++ .gh.deploy.bash
@@ -5,23 +5,16 @@ STARTER_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpr
 echo "Starter version is ${STARTER_VERSION}"
 
 STARTER_JAR_NAME="vertx-starter-${STARTER_VERSION}-fat.jar"
-STARTER_JAR="./build/libs/${STARTER_JAR_NAME}"
+STARTER_JAR="./target/${STARTER_JAR_NAME}"
 
 if [ ! -f ${STARTER_JAR} ]; then
   echo "Could not find file ${STARTER_JAR}"
   exit 1
 fi
 
-REMOTE_USER="jenkins"
-REMOTE_HOSTNAME="start.vertx.io"
-REMOTE_TEMP="/tmp/vertx"
+REMOTE_USER="vertx-deploy"
+REMOTE_HOSTNAME="13.94.149.21"
 
 echo "Sending JAR to ${REMOTE_USER}@${REMOTE_HOSTNAME}..."
-ssh "${REMOTE_USER}"@"${REMOTE_HOSTNAME}" rm -rf "${REMOTE_TEMP}"
-ssh "${REMOTE_USER}"@"${REMOTE_HOSTNAME}" mkdir -p "${REMOTE_TEMP}"
-scp "${STARTER_JAR}" "${REMOTE_USER}"@"${REMOTE_HOSTNAME}":"${REMOTE_TEMP}/${STARTER_JAR_NAME}"
+scp "${STARTER_JAR}" "${REMOTE_USER}"@"${REMOTE_HOSTNAME}":latest-build
 echo "JAR sent"
-
-echo "Deploying with ${REMOTE_USER}@${REMOTE_HOSTNAME}..."
-ssh "${REMOTE_USER}"@"${REMOTE_HOSTNAME}" "sudo -t bash -s" < ./scripts/deploy.sh
-echo "Starter deployed!"
