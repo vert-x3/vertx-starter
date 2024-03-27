604f798f39d1037367a3320eb2852490008585c9
Julien Viet <julien@julienviet.com>
2024-03-27 20:52:59 +0100

    4.5.7 release

--- src/test/java/io/vertx/starter/service/GeneratorTest.java
+++ src/test/java/io/vertx/starter/service/GeneratorTest.java
@@ -122,7 +122,7 @@ class GeneratorTest {
       .setArtifactId("demo")
       .setLanguage(JAVA)
       .setBuildTool(MAVEN)
-      .setVertxVersion("4.5.6")
+      .setVertxVersion("4.5.7")
       .setArchiveFormat(TGZ);
   }
 
